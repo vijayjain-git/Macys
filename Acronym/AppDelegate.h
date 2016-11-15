6c0e4b4c961cd186d8caaf831c385caabe8c8fb8
Vijay Jain <vijayjain@ishans-macbook-air.local>
2016-11-15 10:17:10 -0800

    Initial Commit

---
+++ Acronym/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Acronym
+//
+//  Created by Vijay Jain on 11/15/16.
+//  Copyright © 2016 Vijay Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Acronym/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Acronym
+//
+//  Created by Vijay Jain on 11/15/16.
+//  Copyright © 2016 Vijay Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
